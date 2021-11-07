6a7949f7747c0d7c76fe0620239d0274e97a0bda
Rebecca Lima <56200749+becca-ls@users.noreply.github.com>
2021-11-07 18:33:49 -0300

    Create DFS.cpp

---
+++ Grafos/DFS.cpp
@@ -0,0 +1,14 @@
+//busca em profundidade
+//funcao basica que procura o maior caminho no grafo
+
+void dfs(int i) {
+  count++;
+  visitei[i] = 1;
+  for (auto next : vizinhos[i]) {
+     if (!visitei[next]) {
+        dfs(next, count);
+     }
+  }
+  maior_caminho = max(maior_caminho,count);
+  count--;
+}
