e85381bbacbbf0517ec3a3d92a3945ef047e1e72
Rebecca Lima <56200749+becca-ls@users.noreply.github.com>
2022-02-09 10:22:10 -0300

    Create decimals.cpp

---
+++ adhocs/decimals.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float first, second, third;
+    cin >> first >> second;
+    third = 180 - first - second;
+    cout << std::fixed;
+    cout << std::setprecision(6);
+    cout << "3o angulo=" << third;
+
+
+    return 0;
+}
+
+// This code shows how to print with decimals in cpp
